a5a7180de938f79aa17eaf8a01de59cffc8f2ac5
Curtis Wilson <curtiswemail@gmail.com>
2018-07-10 18:54:41 -0600

    change root view to users/show

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
-  root 'welcome#index'
+  root 'users#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
